93dbd8d63c13ecda0e2a588d5017793c82816a59
ayesha133 <ayesharshad88@gmail.com>
2022-08-20 16:44:32 -0700

    basically almost done

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Quiz from './components/Quiz';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Hellos</Text>
-      <StatusBar style="auto" />
+      <Quiz/>
     </View>
   );
 }
